3a0226297ff931c73a02e7ffce5b25227c283a02
lmuehlen <lmuehlen@uni-muenster.de>
2022-11-13 13:51:00 +0100

    commit data

---
+++ R/fiscalrules.project-package.R
@@ -0,0 +1,15 @@
+#' @import tidyverse
+#' @import fixest
+#' @import plm
+#' @import readxl
+#' @import dotwhisker
+#' @import sf
+#' @import ggthemes
+#' @import ggpubr
+#' @import sandwich
+#' @import lmtest
+#' @import modelsummary
+#' @import plotly
+#' @import stats
+NULL
+#> NULL
