3a160cf6ccf32f8354b5fa95fd2aa660859a9c2d
lmuehlen <lmuehlen@uni-muenster.de>
2023-01-13 16:03:29 +0100

    13.01.2023 16:03

--- R/get_vis_data.R
+++ R/get_vis_data.R
@@ -1,5 +1,6 @@
-get_vis_data<-function(mod,mod_char){
+get_vis_data<-function(mod){
 
+name<-deparse(substitute(mod))
   if(class(mod[[1]])[1]=="pgmm"){
     m<-lapply(mod,
               function(x){
@@ -13,20 +14,24 @@ get_vis_data<-function(mod,mod_char){
   m<-do.call("rbind",m)
 
   m<-m%>%
+    filter(grepl("frd",term))%>%
     tidyr::extract(term,into="term",regex = ".*(\\d+).*")%>%
-    filter(!is.na(term))%>%
-    mutate(term=paste0("Lag: ",term))
-
+    mutate(
+      term=case_when(!is.na(term)~paste0("Lag: ",term),
+                     TRUE~"Lag: 0"
+      )
+    )
 
   m<-m%>%mutate(
-    dep_var=gsub("(.*)_(.*)_(.*)_(.*)_(.*)","\\1",mod_char),
-    level=gsub("(.*)_(.*)_(.*)_(.*)_(.*)","\\2",mod_char),
-    model=gsub("(.*)_(.*)_(.*)_(.*)_(.*)","\\4",mod_char),
+    dep_var=gsub("(.*)_(.*)_(.*)_(.*)_(.*)","\\1",name),
+    rule_type=gsub("(.*)_(.*)_(.*)_(.*)_(.*)","\\2",name),
+    level=gsub("(.*)_(.*)_(.*)_(.*)_(.*)","\\3",name),
+    model=gsub("(.*)_(.*)_(.*)_(.*)_(.*)","\\5",name),
     dynlag=case_when(
-      gsub("(.*)_(.*)_(.*)_(.*)_(.*)","\\5",mod_char)=="dynlag"~"upper bound",
+      gsub("(.*)_(.*)_(.*)_(.*)_(.*)","\\6",name)=="dynlag"~"upper bound",
       TRUE~"lower bound")
   )%>%dplyr::filter(grepl("Lag",m$term))%>%
-    select(term,estimate,conf.low,conf.high,dep_var,level,model,dynlag)
+    select(term,estimate,conf.low,conf.high,dep_var,level,model,dynlag,rule_type)
 
   m$model<-plyr::revalue(m$model,c("within"="Within","iv"="IV","ab"="Arellano-Bond"))
   m$level<-plyr::revalue(m$level,c("sng"="Subnational gov.","lg"="local gov.","rg"="regional gov."))
